9dd695e3c226d6f43de6c59d147a866ed5091983
enzune <theenzune@gmail.com>
2024-11-09 00:17:28 -0700

    Created a menu
    The menu is just one button for now and it needs some more love at the moment.

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+
 public class Main {
-	public static void main(String[] args) {
-    		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        JFrame startPoint = new JFrame();
+        startPoint.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startPoint.setVisible(true);
+        startPoint.setPreferredSize(new Dimension(600,400));
+        startPoint.setMinimumSize(new Dimension(600,400)); // initializing the frame and setting base operations.
+
+        JPanel Grid = new JPanel();
+        startPoint.add(Grid); // Grid needs to be built so that we can display the game space
+
+        JButton reset = new JButton("New Game");
+        startPoint.add(reset); //button needs to be moved off the frame because it is taking up the whole space.
+        reset.setPreferredSize(new Dimension(100,80));
+        reset.addMouseListener(new MouseAdapter() {
+            //add something here for the mouse press to do.
+        });
+    }
 }
