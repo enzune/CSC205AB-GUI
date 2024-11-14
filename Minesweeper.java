f8ee986fc30118297624fb6b37e16ec56e195f23
enzune <theenzune@gmail.com>
2024-11-14 01:31:45 -0700

    Update Minesweeper.java
    added a game frame, and created a 2D array of buttons for the game space.

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,4 +1,4 @@
-// File authored by Jorden Magnuson
+//// File authored by Jorden Magnuson
 // If you make a contribution, don't forget to add your name above.
 import javax.swing.*;
 import java.awt.*;
@@ -8,9 +8,18 @@ import java.awt.event.MouseAdapter;
 
 public class Minesweeper {
 
-    public static void startPanel(){
+    // the frame for the starter.
+    public static JFrame frame = new JFrame("MINESWEEPER");
+    public static Button bombGrid[][]; // this might need to be tweaked insofar as type but for now it's fine.
+    public static int bombs = 16; // The value was decided to be about 20% of board size.
+
+    // This function builds the grid of buttons and decides if they are bombs.
+    public static void buildGrid() {
+        bombGrid = new Button[9][9];
+    }
+
+    public static void startPanel() {
         // I make-a-da-frame-a
-        JFrame frame = new JFrame("MINESWEEPER");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel startPanel = new JPanel();
@@ -20,7 +29,11 @@ public class Minesweeper {
         newGame.setPreferredSize(new Dimension(160, 40));
         newGame.setBackground(Color.green);
         newGame.addActionListener(e -> newBttnClicked());
-        
+
+        /*
+         * leaving this quit button here even though it's kinda useless
+         * because i want to keep the potential for an 'options section open'
+         */
         Button quitButton = new Button("Quit");
         quitButton.setPreferredSize(new Dimension(160, 40));
         quitButton.setBackground(Color.red);
@@ -33,16 +46,26 @@ public class Minesweeper {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-
+    /*
+     * This function creates a new game frame once new game is clicked. 
+     * It then generates a grid of buttons and hides bombs among them.
+     */
     public static void newBttnClicked() {
-        System.out.println("starting new game!");
+        frame.dispose();
+        GridLayout gameGrid = new GridLayout(16,16);
+        
+        // the gameFrame needs some more tweaks but that comes after the grid is finalized.
+        JFrame gameFrame = new JFrame("MINESWEEPER");
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gameFrame.setLayout(gameGrid);
+
+        JPanel gamePanel = new JPanel();
+        gameFrame.add(gamePanel);
+        gameFrame.pack();
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setVisible(true);
     }
 
-    public static void quitGame() {
-        System.exit(0);
-    }
-    
-    public static void main(String[] args) {
-        startPanel();
-    }
+    public static void quitGame() {System.exit(0);}
+    public static void main(String[] args) {startPanel();}
 }
